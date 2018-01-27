9e501573709bf8f01993151de6e839a9e5c48c0d
unknown <pearcewilliam222@gmail.com>
2018-01-26 20:55:17 -0400

    Removed unicode from check

--- bot.py
+++ bot.py
@@ -18,12 +18,12 @@ async def on_ready(): #prints log in info
 
 @client.event
 async def on_message(message):
-        if "@everyone" in message.content: #deletes any message that contains the string @everyone
+        if "@everyone".lower() in message.content: #deletes any message that contains the string @everyone
                 await client.send_message(message.channel, "@ everyone is disabled on this server")
                 await client.delete_message(message)
 
         for word in profs:
-                if word in message.content.lower(): #deletes any message that contains profanity
+                if word in message.content.join([x if ord(x) < 128 for x in string]).lower(): #deletes any message that contains profanity
                         await client.send_message(message.channel, "profanities are not allowed on this server")
                         await client.delete_message(message)
 
--- token1.py
+++
@@ -1,1 +0,0 @@
-tok = "NDA2MjcyMDEyMDk3NjgzNDU2.DUwiKw.RZtb93MM6CBUzwga9pN8efsbFDA"
